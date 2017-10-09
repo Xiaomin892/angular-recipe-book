5b43bee8fb7c739ff78f6d895867108f6e3508cb
Vladyslav Burdeniuk <Vladyslav.Burdeniuk@3shape.com>
2017-10-09 17:53:38 +0300

    Divide codebase to separate modules and enable lazy loading of recipes module

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,9 +1,9 @@
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService implements CanActivate, CanLoad {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
@@ -12,4 +12,12 @@ export class AuthGuardService implements CanActivate {
     }
     return this.authService.isAuthenticated();
   }
+
+  canLoad(route: Route) {
+    if (!this.authService.isAuthenticated()) {
+      this.router.navigate(['/signin']);
+    }
+    return this.authService.isAuthenticated();
+  }
+
 }
