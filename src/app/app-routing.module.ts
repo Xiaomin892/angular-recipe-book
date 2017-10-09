0238941b01e14d00aa8ac0ad1bffa044e16e411d
Vladyslav Burdeniuk <Vladyslav.Burdeniuk@3shape.com>
2017-10-09 18:06:05 +0300

    Divide codebase to separate modules and enable lazy loading of recipes module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { AuthGuardService } from './auth/auth-guard.service';
 import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuardService] },
   { path: 'recipes', loadChildren: './recipes/recipe.module#RecipeModule', canLoad: [AuthGuardService] },
-  { path: 'shopping-list', component: ShoppingListComponent, canActivate: [AuthGuardService] }
+  { path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule', canLoad: [AuthGuardService] }
 ];
 
 @NgModule({
