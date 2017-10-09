3eccc67f3fb221a3a317d9ce590a19e8b5233056
Vladyslav Burdeniuk <Vladyslav.Burdeniuk@3shape.com>
2017-10-09 18:07:12 +0300

    Clean up import after enabling lazy load of shopping-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { DataStorageService } from './shared/data-storage.service';
 import { AuthService } from './auth/auth.service';
 import { AuthGuardService } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 import { HomeComponent } from './home/home.component';
 
